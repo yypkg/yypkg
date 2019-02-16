496cba0edc9a9f8da54d585a22229ce83af5c194
刘达慰 <liudawei@yy.com>
2019-02-16 12:34:12 +0800

    feat:add source loader

---
+++ core/source-loader.js
@@ -0,0 +1,202 @@
+/**
+ * 资源加载器（image/css/js/svga）
+ *
+ * @param {Array} url 资源路径数组
+ * @param {Number} retry 加载失败时的重试次数
+ * @param {Boolean} autoStart 是否在实例化时自动执行start()
+ * @return {Function} SourceLoader
+ */
+
+let loadData = {}
+
+// 事件定义
+let $onEvent = {
+  process: () => {},
+  complete: () => {},
+  error: () => {},
+}
+
+let SourceLoader = class SourceLoader {
+
+  constructor (argument) {
+    this.init(argument)
+  }
+
+  init (config) {
+    // 默认配置
+    let defaults = {
+      url: [],
+      retry: 3,
+      autoStart: false,
+    }
+    this.configs = this.extend(defaults, config)
+    this.configs.autoStart && this.start()
+  }
+
+  // 加载器
+  loader () {
+    const script = (url, requestCounts)=>{
+      const scriptElement = document.createElement('script')
+      scriptElement.src = url
+      document.getElementsByTagName('head')[ 0 ].appendChild(scriptElement)
+      this.loadendEvent(scriptElement, url, 'script', requestCounts)
+    }
+
+    const style = (url, requestCounts)=> {
+      const styleElement = document.createElement('link')
+      styleElement.type = 'text/css'
+      styleElement.rel = 'stylesheet'
+      styleElement.href = url
+      document.getElementsByTagName('head')[ 0 ].appendChild(styleElement)
+      this.loadendEvent(styleElement, url, 'style', requestCounts)
+    }
+
+    const image = (url, requestCounts)=> {
+      let img = new Image()
+      img.src = url
+      this.loadendEvent(img, url, 'image', requestCounts)
+      img = null
+    }
+
+    const svga = (url, requestCounts)=> {
+      let xhr = new XMLHttpRequest()
+      xhr.open('GET', url, true)
+      xhr.responseType = 'arraybuffer'
+      xhr.send()
+      this.loadendEvent(xhr, url, 'svga', requestCounts)
+      xhr = null
+    }
+
+    return {
+      script,
+      style,
+      image,
+      svga,
+    }
+  }
+
+  /**
+   * loadend事件处理
+   *
+   * @param {object} node 节点对象
+   * @param {String} url 当前加载的资源url
+   * @param {String} type 资源类型 "script||style||image||svga"
+   * @param {Number} requestCounts 当前请求的次数
+   * @return {Funtion} 事件处理函数
+   */
+  loadendEvent (node, url, type, requestCounts) {
+    let error = node.onerror = node.ontimeout = () => {
+      // 加载失败则重试
+      if (requestCounts <= this.configs.retry) {
+        requestCounts++
+        this.loader()[type](url, requestCounts)
+      } else {
+        return this.errorHandle(url)
+      }
+    }
+    node.onload = () => {
+      let onload = null
+      if (type !== 'svga') {
+        onload = this.successHandle(url)
+        return onload
+      }
+      if ((node.status >= 200 && node.status < 300) || node.status === 304) {
+        onload = this.successHandle(url)
+        return onload
+      } else {
+        error()
+      }
+    }
+  }
+
+  errorHandle (url) {
+    console.error(`${url} load fail`)
+    $onEvent.error(url)
+    ++loadData.count
+  }
+
+  successHandle (url) {
+    ++loadData.count
+  }
+
+  start () {
+    const urls = this.configs.url
+    let urlsArray = []
+    let tmpCount = 0
+    typeof urls === 'string' ? urlsArray.push(urls) : urlsArray = urls
+    urlsArray.forEach((item, index) => {
+      let extName = item.split('.').pop().toLowerCase()
+      if (extName.indexOf('?') > -1) {
+        extName = extName.split('?')[0].toLowerCase()
+      }
+      let loaderName = ''
+      switch (extName) {
+        case 'css':
+          loaderName = 'style'
+          break
+
+        case 'js':
+          loaderName = 'script'
+          break
+
+        case 'jpg':
+        case 'jpeg':
+        case 'png':
+        case 'gif':
+        case 'webp':
+        case 'mbp':
+          loaderName = 'image'
+          break
+
+        case 'svga':
+          loaderName = 'svga'
+          break
+      }
+      this.loader()[loaderName](item, 0)
+    })
+
+    // 通过监听loadData.count判断加载状态
+    Object.defineProperty(loadData, 'count', {
+      get () {
+        return tmpCount
+      },
+      set (value) {
+        tmpCount = value
+        if (tmpCount === urlsArray.length) {
+          $onEvent.complete(tmpCount)
+        }
+        else {
+          $onEvent.process(urlsArray.length, tmpCount, urlsArray[tmpCount])
+        }
+      }
+    })
+  }
+
+  $on (type, fun) {
+    $onEvent[type] = fun
+  }
+
+  /**
+   * 对象合并
+   *
+   * @param {object} target 原对象
+   * @param {object} options 新对象
+   * @return {object} 合并后的对象
+   */
+  extend (target, options) {
+    let copy = null
+    for (var name in options) {
+      copy = options[name]
+
+      //防止循环调用
+      if (target === copy) continue
+
+      //防止附加未定义值
+      if (typeof copy === 'undefined') continue
+      target[name] = copy
+    }
+    return target
+  }
+}
+
+export default SourceLoader
---
+++ docs/source-loader.md
@@ -0,0 +1,48 @@
+# SourceLoader
+
+资源加载器，常用于资源预加载，支持的资源有：css、js、img（jpg、jpeg、png、gif、webp、mbp）、svga
+
+## 使用
+
+```js
+import SourceLoader from 'yypkg/source-loader'
+const resources = ['./demo.css', './demo.js']
+const loader = new SourceLoader({
+  url: resources,
+  retry: 3,
+})
+loader.start()
+loader.$on('process', (total, count, url) => {
+  console.log(total, count, url)
+})
+loader.$on('complete', (total) => {
+  console.log('total', total)
+})
+loader.$on('error', (url) => {
+  console.error('加载失败', url)
+})
+```
+
+## 配置项 `options`
+
+| 参数名 | 描述 | 类型 | 默认值 |
+|-|-|-|-|
+| url | 需要加载的资源url | String 或 Array | `''` |
+| retry | 加载失败时的重试次数 | Number | `3` |
+| autoStart | 是否在实例化时自动执行start() | Boolean | `false` |
+
+## 方法
+
+| 方法名 | 描述 | 参数 |
+|-|-|-|-|
+| start | 开始触发资源加载 | 无 |
+
+
+## 事件
+
+| 事件名 | 描述 | 参数 |
+|-|-|-|-|
+| process | 资源加载过程中 | total：资源总数 <br> count：当前加载资源的索引值 <br> url：当前加载资源的url  |
+| complete | 资源加载完毕（无论加载失败还是成功） | total：资源总数  |
+| error | 资源加载失败（retry不触发）| url：加载失败的资源url |
+
