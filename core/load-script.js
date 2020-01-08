1b74cac82789b4d5d16c72757d5c0f72113329ce
liudawei <liudawei@yy.com>
2020-01-08 15:22:14 +0800

    fix: load-style 与 load-script 模块更新，新增失败后重试的功能

--- core/load-script.js
+++ core/load-script.js
@@ -2,18 +2,29 @@
  * 动态加载脚本
  *
  * @param {String} url
- * @return {Promise}
+ * @param {Integer} retry 加载失败时重试的次数，大于等于1的正整数
+ * @return {Promise} true为最终加载成功，false为最终加载失败
  */
-const loadScript = function (url) {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script')
 
-    script.src = url
+const fetch = (url, retry, resolve)=> {
+  const script = document.createElement('script')
+  script.src = url
+  script.onload = ()=> {
+    resolve(true)
+  }
+  script.onerror = script.ontimeout = async ()=> {
+    if(retry && typeof retry === 'number' && retry >= 1) {
+      fetch(url, retry - 1, resolve)
+    } else {
+      resolve(false)
+    }
+  }
+  window.document.head.appendChild(script)
+}
 
-    script.onload = resolve
-    script.onerror = reject
-
-    window.document.head.appendChild(script)
+function loadScript(url, retry) {
+  return new Promise((resolve) => {
+    fetch(url, retry, resolve)
   })
 }
 
--- core/load-style.js
+++ core/load-style.js
@@ -1,21 +1,32 @@
 /**
- * 动态加载样式
+ * 动态加载样式表
  *
  * @param {String} url
- * @return {Promise}
+ * @param {Integer} retry 加载失败时重试的次数，大于等于1的正整数
+ * @return {Promise} true为最终加载成功，false为最终加载失败
  */
-const loadStyle = function (url) {
-  return new Promise((resolve, reject) => {
-    const link = document.createElement('link')
-    link.rel = 'stylesheet'
-    link.type = 'text/css'
 
-    link.href = url
+const fetch = (url, retry, resolve)=> {
+  const link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.type = 'text/css'
+  link.href = url
+  link.onload = ()=> {
+    resolve(true)
+  }
+  link.onerror = link.ontimeout = async ()=> {
+    if(retry && typeof retry === 'number' && retry >= 1) {
+      fetch(url, retry - 1, resolve)
+    } else {
+      resolve(false)
+    }
+  }
+  window.document.head.appendChild(link)
+}
 
-    link.onload = resolve
-    link.onerror = reject
-
-    window.document.head.appendChild(link)
+function loadStyle(url, retry) {
+  return new Promise((resolve) => {
+    fetch(url, retry, resolve)
   })
 }
 
